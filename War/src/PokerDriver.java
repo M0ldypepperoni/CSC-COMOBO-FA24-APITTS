50ebf1e7835a4da8529c9a4c62d47e45134207de
A P <averyp5753@gmail.com>
2024-11-13 07:47:58 -0700

    7:47am 11/13/2024

--- War/src/PokerDriver.java
+++ War/src/PokerDriver.java
@@ -8,6 +8,7 @@ public class PokerDriver {
 		Random rand = new Random();
 		int count = 1;
 		char again = 'y';
+		// array for points maybe?
 		// TODO add file writer and reader for high-score
 		Scanner input = new Scanner(System.in);
 		Pile player = new Pile();
@@ -32,19 +33,25 @@ public class PokerDriver {
 
 		System.out.println("Please enter the amount of bots you want to play against.");
 		botAmount = input.nextInt();
-		ArrayList<ArrayList<Card>> piles = new ArrayList<ArrayList<Card>>(botAmount);
+		ArrayList<Pile> piles = new ArrayList<Pile>(botAmount);
 // TODO add piles in switch statement to piles
 		while (again == 'y' || again == 'Y') {
 			switch (botAmount) {
 			case 1:
 				player.addDeck(deck.subdeck(0, 1));
 				b1.addDeck(deck.subdeck(2, 3));
+				piles.add(player);
+				piles.add(b1);
+//				piles.get(0).addDeck(deck.subdeck(playerPoints, botAmount));
 				left.addDeck(deck.subdeck(4, 51));
 				break;
 			case 2:
 				player.addDeck(deck.subdeck(0, 1));
 				b1.addDeck(deck.subdeck(2, 3));
 				b2.addDeck(deck.subdeck(4, 5));
+				piles.add(player);
+				piles.add(b1);			
+				piles.add(b2);
 				left.addDeck(deck.subdeck(6, 51));
 				break;
 			case 3:
@@ -52,6 +59,10 @@ public class PokerDriver {
 				b1.addDeck(deck.subdeck(2, 3));
 				b2.addDeck(deck.subdeck(4, 5));
 				b3.addDeck(deck.subdeck(6, 7));
+				piles.add(player);
+				piles.add(b1);
+				piles.add(b2);
+				piles.add(b3);
 				left.addDeck(deck.subdeck(8, 51));
 				break;
 			case 4:
@@ -60,82 +71,138 @@ public class PokerDriver {
 				b2.addDeck(deck.subdeck(4, 5));
 				b3.addDeck(deck.subdeck(6, 7));
 				b4.addDeck(deck.subdeck(8, 9));
+				piles.add(player);
+				piles.add(b1);
+				piles.add(b2);
+				piles.add(b3);
+				piles.add(b4);
 				left.addDeck(deck.subdeck(10, 51));
 				break;
 			case 5:
 				player.addDeck(deck.subdeck(0, 1));
+				piles.add(player);
 				b1.addDeck(deck.subdeck(2, 3));
+				piles.add(b1);
 				b2.addDeck(deck.subdeck(4, 5));
+				piles.add(b2);
 				b3.addDeck(deck.subdeck(6, 7));
+				piles.add(b3);
 				b4.addDeck(deck.subdeck(8, 9));
+				piles.add(b4);
 				b5.addDeck(deck.subdeck(10, 11));
+				piles.add(b5);
 				left.addDeck(deck.subdeck(12, 51));
 				break;
 			case 6:
 				player.addDeck(deck.subdeck(0, 1));
+				piles.add(player);
 				b1.addDeck(deck.subdeck(2, 3));
+				piles.add(b1);
 				b2.addDeck(deck.subdeck(4, 5));
+				piles.add(b2);
 				b3.addDeck(deck.subdeck(6, 7));
+				piles.add(b3);
 				b4.addDeck(deck.subdeck(8, 9));
+				piles.add(b4);
 				b5.addDeck(deck.subdeck(10, 11));
+				piles.add(b5);
 				b6.addDeck(deck.subdeck(12, 13));
+				piles.add(b6);
 				left.addDeck(deck.subdeck(14, 51));
 				break;
 			case 7:
 				player.addDeck(deck.subdeck(0, 1));
+				piles.add(player);
 				b1.addDeck(deck.subdeck(2, 3));
+				piles.add(b1);
 				b2.addDeck(deck.subdeck(4, 5));
+				piles.add(b2);
 				b3.addDeck(deck.subdeck(6, 7));
+				piles.add(b3);
 				b4.addDeck(deck.subdeck(8, 9));
+				piles.add(b4);
 				b5.addDeck(deck.subdeck(10, 11));
+				piles.add(b5);
 				b6.addDeck(deck.subdeck(12, 13));
+				piles.add(b6);
 				b7.addDeck(deck.subdeck(14, 15));
+				piles.add(b7);
 				left.addDeck(deck.subdeck(16, 51));
 				break;
 			case 8:
 				player.addDeck(deck.subdeck(0, 1));
+				piles.add(player);
 				b1.addDeck(deck.subdeck(2, 3));
+				piles.add(b1);
 				b2.addDeck(deck.subdeck(4, 5));
+				piles.add(b2);
 				b3.addDeck(deck.subdeck(6, 7));
+				piles.add(b3);
 				b4.addDeck(deck.subdeck(8, 9));
+				piles.add(b4);
 				b5.addDeck(deck.subdeck(10, 11));
+				piles.add(b5);
 				b6.addDeck(deck.subdeck(12, 13));
+				piles.add(b6);
 				b7.addDeck(deck.subdeck(14, 15));
+				piles.add(b7);
 				b8.addDeck(deck.subdeck(16, 17));
+				piles.add(b8);
 				left.addDeck(deck.subdeck(18, 51));
 				break;
 			case 9:
 				player.addDeck(deck.subdeck(0, 1));
+				piles.add(player);
 				b1.addDeck(deck.subdeck(2, 3));
+				piles.add(b1);
 				b2.addDeck(deck.subdeck(4, 5));
+				piles.add(b2);
 				b3.addDeck(deck.subdeck(6, 7));
+				piles.add(b3);
 				b4.addDeck(deck.subdeck(8, 9));
+				piles.add(b4);
 				b5.addDeck(deck.subdeck(10, 11));
+				piles.add(b5);
 				b6.addDeck(deck.subdeck(12, 13));
+				piles.add(b6);
 				b7.addDeck(deck.subdeck(14, 15));
+				piles.add(b7);
 				b8.addDeck(deck.subdeck(16, 17));
+				piles.add(b8);
 				b9.addDeck(deck.subdeck(18, 19));
+				piles.add(b9);
 				left.addDeck(deck.subdeck(20, 51));
 				break;
 			case 10:
 				player.addDeck(deck.subdeck(0, 1));
+				piles.add(player);
 				b1.addDeck(deck.subdeck(2, 3));
+				piles.add(b1);
 				b2.addDeck(deck.subdeck(4, 5));
+				piles.add(b2);
 				b3.addDeck(deck.subdeck(6, 7));
+				piles.add(b3);
 				b4.addDeck(deck.subdeck(8, 9));
+				piles.add(b4);
 				b5.addDeck(deck.subdeck(10, 11));
+				piles.add(b5);
 				b6.addDeck(deck.subdeck(12, 13));
+				piles.add(b6);
 				b7.addDeck(deck.subdeck(14, 15));
+				piles.add(b7);
 				b8.addDeck(deck.subdeck(16, 17));
+				piles.add(b8);
 				b9.addDeck(deck.subdeck(18, 19));
+				piles.add(b9);
 				b10.addDeck(deck.subdeck(20, 21));
+				piles.add(b10);
 				left.addDeck(deck.subdeck(22, 51));
 				break;
 			default:
 				System.out.println("too many");
 				break;
 			}
-
+			System.out.println(piles.get(0));
 			left.shufflePile();
 			if (count == 1) {
 // TODO for little and big blind use rand int for if and else move left one
